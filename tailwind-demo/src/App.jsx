9e397d0ba1b4e31c4b026347fa97d1952c108fab
Gunjan Kumar <gunjan.kumar@cyphel.com>
2025-05-28 09:04:04 +0100

    tailwind demo

---
+++ tailwind-demo/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl text-amber-200 font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
